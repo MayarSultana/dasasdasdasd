4eac2f966219e90c6464955478b697c388c4de34
MayarSultana <mayarsultana2@gmail.com>
2025-01-30 17:05:30 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    EasyLocalization(
+        supportedLocales: [Locale('en'), Locale('ar')],
+        path:
+            'assets/translations', // <-- change the path of the translation files
+        fallbackLocale: Locale('en'),
+        child: MyApp()),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(tr("title"))),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('n_books'.plural(0)),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              context.setLocale(Locale("ar"));
+            },
+            child: Text("switch to arabic"),
+          ),
+          SizedBox(height: 12),
+          ElevatedButton(
+            onPressed: () {
+              context.setLocale(Locale("en"));
+            },
+            child: Text("witch to english"),
+          ),
+        ],
+      ),
+    );
+  }
+}
